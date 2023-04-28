8cb11c5b0ee8a9ea8622bef1617c47d11f1e31fd
sethu <ssethucse@gmail.com>
2023-04-28 09:11:06 +0530

    django framework

--- movies/settings.py
+++ movies/settings.py
@@ -23,14 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r&kv*cgq5=od=0(r#m_#sr13yuz8lo7lou2gzj*xupy17$i2*7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'movies',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ movies/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render
+from .models import Movie
+
+def movies(request):
+    data = Movie.objects.all()
+    return render(request,'movies/movies.html',{'movies':data})
+
+def home(request):
+    return HttpResponse("Homes")
+
+def detail(request, id):
+    data = Movie.objects.get(pk=id)
+    return render(request,'movies/detail.html',{'movies':data})
+
+def add(request):
+    title = request.POST.get('title')
+    year = request.POST.get('year')
+
+    if title and year:
+        movie = Movie(title = title,year = year)
+        movie.save()
+        return HttpResponseRedirect('/movies')
+    return render(request,'movies/add.html')
+
+def delete(request,id):
+    try:
+        movie = Movie.objects.get(pk=id)
+    except:
+        raise Http404('Movie Does not Exist')
+    movie.delete()
+    return HttpResponseRedirect('/movies')
